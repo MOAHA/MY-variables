832b4a95237963da1d6a958307910f94db5f7782
korek pc 4 <korek pc 4@KOREKPC4-HP>
2017-09-20 12:38:16 +0300

    Add project files.

---
+++ MY variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MY_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Muhnnd Variable;
+            int xy = 90;
+            Console.WriteLine("IS variable MUHNND = {0}",xy);
+            Console.ReadKey();
+        }
+    }
+}
